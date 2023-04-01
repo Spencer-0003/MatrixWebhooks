0458b566925a55b356c20515cbb92c328b9aa3be
Spencer-0003 <git@spencer.moe>
2023-04-01 08:17:08 +0100

    Packages: Setup linting.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@
 
 import 'module-alias/register';
 import 'dotenv/config';
+import fastify from 'fastify';
+
+// Create server
+const server = fastify()
+  .register(import('@fastify/helmet'))
+  .get('/', () => 'Ready to receive events.');
+
+// Listen
+server.listen({ host: '0.0.0.0', port: process.env.PORT ?? 3000 }, err => console.log(`[MatrixWebhooks]: ${err ? 'Port in use.' : 'Server listening on designated port.'}`));
--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
     ACCESS_TOKEN: string;
     DATABASE_URL: string;
     HOMESERVER: string;
+    PORT?: number;
     SHADOW_DATABASE_URL: string;
   }
 }
