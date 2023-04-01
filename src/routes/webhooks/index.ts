0aa979330a0a5a565821e41975e95647cc24afdd
Spencer <git@spencer.moe>
2023-04-01 11:05:22 +0100

    Chore(Lint): Disable rule JS-0376.

--- src/routes/webhooks/index.ts
+++ src/routes/webhooks/index.ts
@@ -5,6 +5,7 @@ import type { webhookParameters } from './schemas';
 import { db } from '@classes/Database';
 
 // Endpoints
+// skipcq: JS-0376
 export default async function (server: FastifyInstance) {
 	server.get('/:token', async (req, res) => {
 		const webhook = await db.getWebhook((req.params as webhookParameters).token);
