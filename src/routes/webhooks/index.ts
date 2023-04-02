8d4d8c14935e975d541503da0ed49e9c02bf9d73
Spencer <git@spencer.moe>
2023-04-02 01:10:22 +0100

    Security(Webhooks): Add secrets to prevent misuse.

--- src/routes/webhooks/index.ts
+++ src/routes/webhooks/index.ts
@@ -16,10 +16,13 @@ export default async function (server: FastifyInstance) {
 
 	server.post('/:token', async (req, res) => {
 		const webhook = await db.getWebhook((req.params as webhookParameters).token);
-		const content = (req.body as Record<string, string>).content;
+		const body = (req.body as Record<string, string>);
+		const content = body.content;
 
 		if (!webhook || !content)
 			return res.send({ error: !webhook ? 'Invalid webhook.' : 'You didn\'t specify any content.' });
+		else if (webhook.secret && body.secret !== webhook.secret)
+			return res.send({ error: 'This webhook is protected with a secret.' });
 
 		client.sendMessage(webhook.roomId, { body: content, msgtype: 'm.text' });
 		return res.send('OK');
