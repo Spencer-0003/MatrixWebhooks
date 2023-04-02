49090fd458fdd99ca262740f734cfe20e49c2e30
Spencer <git@spencer.moe>
2023-04-02 05:22:07 +0100

    [skip ci] Disable check.

--- src/events/room.failed_decryption.ts
+++ src/events/room.failed_decryption.ts
@@ -2,6 +2,7 @@
 import { Event } from '@classes/Event';
 
 // Export class
+// skipcq: JS-0105
 export class FailedDecryption extends Event {
   // Docs say to use "any", ew: https://turt2live.github.io/matrix-bot-sdk/tutorial-bot.html
   run(roomId: string, _: any, e: Error): void {
