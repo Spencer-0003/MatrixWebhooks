6ae8b5ee7e99385e4397608098ed042fb6993ba3
Spencer <git@spencer.moe>
2023-04-02 06:45:26 +0100

    Event(RoomLeave): Clear webhooks on kick.

---
+++ src/events/room.leave.ts
@@ -0,0 +1,10 @@
+// Import classes
+import { Event } from '@classes/Event';
+
+// Export class
+export class RoomLeave extends Event {
+  async run(roomId: string): Promise<void> {
+    await this.client.db.clearRoom(roomId);
+    console.log(`[MatrixWebhooks]: Removed from ${roomId}}, cleared webhooks.`);
+  }
+}
