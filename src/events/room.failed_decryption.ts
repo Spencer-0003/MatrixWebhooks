86594b7c505a6c5352c28043a25a5eaaf5645a92
Spencer <git@spencer.moe>
2023-07-08 20:15:26 +0100

    Fixed anti-patterns?

--- src/events/room.failed_decryption.ts
+++ src/events/room.failed_decryption.ts
@@ -2,9 +2,9 @@
 import { Event } from '@classes/Event';
 
 // Export class
-// skipcq: JS-0105
 export class FailedDecryption extends Event {
   // Docs say to use "any", ew: https://turt2live.github.io/matrix-bot-sdk/tutorial-bot.html
+  // skipcq: JS-0105 JS-0323
   run(roomId: string, _: any, e: Error): void {
     console.log(`[MatrixWebhooks]: Failed to decrypt message in ${roomId}, error:\n${e}`);
   }
