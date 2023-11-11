08b14b903fe21f35aa8f3ec3595212c0b0ab71b5
Spencer <git@spencer.moe>
2023-11-11 17:46:30 +0000

    Add debug mode option.

--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     ACCESS_TOKEN: string;
     DATABASE_URL: string;
+    DEBUG_MODE: string;
     DOMAIN: string;
     HOMESERVER: string;
     HOMESERVER_RESTRICTED: boolean?;
